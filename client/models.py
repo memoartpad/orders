fefd19d845800a96a7970b841cce7901aa691c29
Guillermo Arteaga Padilla <memoartpad80@gmail.com>
2020-09-24 01:26:16 -0500

    Client model added

--- client/models.py
+++ client/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    TYPE_CHOICES = (
+    ("NORMAL", "Normal"),
+    ("PLATA", "Plata"),
+    ("ORO", "Oro"),
+    ("PLATINO", "Platino"),
+)
+    name = models.CharField(max_length=64, blank=False)
+    code = models.CharField(max_length=64, blank=False)
+    photo = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    month = models.CharField(max_length=7, choices=TYPE_CHOICES, default="NORMAL")
