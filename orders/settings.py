de68130a5b9154831336f87f343d8de11846d17c
Guillermo Arteaga Padilla <memoartpad80@gmail.com>
2020-09-24 01:40:16 -0500

    Vendor model added

--- orders/settings.py
+++ orders/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'client',
-    'article'
+    'article',
+    'vendor'
 ]
 
 MIDDLEWARE = [
--- vendor/models.py
+++ vendor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from article.models import Article
 
-# Create your models here.
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    address = models.CharField(max_length=128)
+    articles = models.ManyToManyField(Article)
